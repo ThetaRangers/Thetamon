ba748c97de09e1762676773955462900f3b12443
Lorenzo Valeriani <lorenzoval98@gmail.com>
2020-05-30 19:30:44 +0200

    Add FileUnzipper
    Signed-off-by: Lorenzo Valeriani <lorenzoval98@gmail.com>

--- app/src/main/java/it/thetarangers/thetamon/activities/MainActivity.java
+++ app/src/main/java/it/thetarangers/thetamon/activities/MainActivity.java
@@ -8,25 +8,51 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.widget.TextView;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
 
 import it.thetarangers.thetamon.R;
 import it.thetarangers.thetamon.database.PokemonDao;
 import it.thetarangers.thetamon.database.PokemonDb;
 import it.thetarangers.thetamon.model.Pokemon;
 import it.thetarangers.thetamon.utilities.FileDownloader;
+import it.thetarangers.thetamon.utilities.FileUnzipper;
 
 public class MainActivity extends AppCompatActivity {
 
+    BroadcastReceiver onComplete;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // TODO TODO TODO refactor and handle orientation change (ffs)
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Handler h = new Handler();
+        final Runnable update = new Runnable() {
+            @Override
+            public void run() {
+                ((TextView) findViewById(R.id.tv_hello)).setText("Unzip Completed");
+            }
+        };
+        final Thread t = new Thread() {
+            @Override
+            public void run() {
+                unpack();
+                h.post(update);
+            }
+        };
 
-        BroadcastReceiver onComplete = new BroadcastReceiver() {
+        onComplete = new BroadcastReceiver() {
             public void onReceive(Context context, Intent intent) {
                 ((TextView) findViewById(R.id.tv_hello)).setText("Download Completed");
+                // TODO calculate and check md5
+                t.start();
             }
         };
 
@@ -48,6 +74,26 @@ public class MainActivity extends AppCompatActivity {
         dao.insertPokemon(new Pokemon(1, "charmander"));
 
         Log.w("POKE", dao.getPokemons().get(0).name);
+    }
+
+    private void unpack() {
+        File file = new File(getApplicationContext().getExternalFilesDir(null),
+                "Sprites/sprites.zip");
+        FileUnzipper fu = new FileUnzipper();
+        fu.unzip(file, getApplicationContext().getFilesDir().getAbsolutePath());
+        try {
+            Log.d("POKE", "Cleaning up");
+            FileUtils.forceDelete(Objects.requireNonNull(getApplicationContext()
+                    .getExternalFilesDir(null)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        unregisterReceiver(onComplete);
+        super.onDestroy();
     }
 
 }
---
+++ app/src/main/java/it/thetarangers/thetamon/utilities/FileUnzipper.java
@@ -0,0 +1,38 @@
+package it.thetarangers.thetamon.utilities;
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+public class FileUnzipper {
+
+    public void unzip(File file, String outputDir) {
+        try (java.util.zip.ZipFile zipFile = new ZipFile(file)) {
+            Enumeration<? extends ZipEntry> entries = zipFile.entries();
+            while (entries.hasMoreElements()) {
+                ZipEntry entry = entries.nextElement();
+                File entryDestination = new File(outputDir,  entry.getName());
+                if (entry.isDirectory()) {
+                    entryDestination.mkdirs();
+                } else {
+                    entryDestination.getParentFile().mkdirs();
+                    try (InputStream in = zipFile.getInputStream(entry);
+                         OutputStream out = new FileOutputStream(entryDestination)) {
+                        IOUtils.copy(in, out);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
