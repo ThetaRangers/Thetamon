8881d68b1838e2a6b74bdd5157082ab8403ebd2e
Lorenzo Valeriani <lorenzoval98@gmail.com>
2020-06-12 15:26:39 +0200

    Add Pokemon moves as BottomSheetDialog
    Signed-off-by: Lorenzo Valeriani <lorenzoval98@gmail.com>

--- app/src/main/java/it/thetarangers/thetamon/activities/PokemonDetailActivity.java
+++ app/src/main/java/it/thetarangers/thetamon/activities/PokemonDetailActivity.java
@@ -1,29 +1,23 @@
 package it.thetarangers.thetamon.activities;
 
-import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
+import android.os.Handler;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.android.material.card.MaterialCardView;
 import com.google.android.material.imageview.ShapeableImageView;
 import com.google.android.material.shape.CornerFamily;
 
 import java.util.List;
 
 import it.thetarangers.thetamon.R;
-import it.thetarangers.thetamon.adapters.PokedexAdapter;
+import it.thetarangers.thetamon.database.DaoThread;
+import it.thetarangers.thetamon.fragments.FragmentMoves;
 import it.thetarangers.thetamon.model.Move;
 import it.thetarangers.thetamon.model.Pokemon;
 import it.thetarangers.thetamon.utilities.ImageManager;
@@ -33,6 +27,8 @@ import it.thetarangers.thetamon.utilities.VolleyPokemonDetail;
 
 public class PokemonDetailActivity extends AppCompatActivity {
     Pokemon pokemon;
+    Handler handler;
+    List<Move> moves;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,10 +37,11 @@ public class PokemonDetailActivity extends AppCompatActivity {
 
         pokemon = getIntent().getParcelableExtra("pokemon");
 
+        handler = new Handler();
         new Holder();
     }
 
-    class Holder {
+    class Holder implements View.OnClickListener {
         final ImageView ivSprite;
         final ShapeableImageView ivOverlay;
         final ShapeableImageView ivBackground;
@@ -52,11 +49,12 @@ public class PokemonDetailActivity extends AppCompatActivity {
         final TextView tvID;
         final TextView tvHabitat;
         final TextView tvHp;
-        final RecyclerView rvMoves;
 
         final TextView tvLoading, tvType1, tvType2;
 
-        ImageManager imageManager = new ImageManager();
+        final Button btnMoves;
+
+        ImageManager imageManager;
 
         Holder() {
             tvID = findViewById(R.id.tvId);
@@ -66,11 +64,16 @@ public class PokemonDetailActivity extends AppCompatActivity {
             ivSprite = findViewById(R.id.ivSprite);
             ivOverlay = findViewById(R.id.ivOverlay);
             tvHp = findViewById(R.id.tvHp);
-            rvMoves = findViewById(R.id.rvMoves);
 
             tvLoading = findViewById(R.id.tvLoading);
             tvType1 = findViewById(R.id.tvType1);
             tvType2 = findViewById(R.id.tvType2);
+
+            btnMoves = findViewById(R.id.btnMoves);
+            btnMoves.setOnClickListener(this);
+            btnMoves.setEnabled(false);
+
+            imageManager = new ImageManager();
 
             // set everything before having the detail of the pokemon
             this.beforeDetails();
@@ -89,10 +92,6 @@ public class PokemonDetailActivity extends AppCompatActivity {
         }
 
         private void beforeDetails() {
-            Context context = PokemonDetailActivity.this;
-
-            //set recycler view layout manager
-            rvMoves.setLayoutManager(new LinearLayoutManager(context));
 
             //set textViews
             tvID.setText(String.format("#%d", pokemon.getId()));
@@ -107,15 +106,15 @@ public class PokemonDetailActivity extends AppCompatActivity {
             ivBackground.setBackgroundColor(Color.parseColor(pokemon.getAverageColor()));
 
             ivSprite.setImageBitmap(imageManager.loadFromDisk(
-                    context.getFilesDir() + context.getString(R.string.sprites_front),
-                    pokemon.getId() + context.getString(R.string.extension)));
+                    PokemonDetailActivity.this.getFilesDir() + PokemonDetailActivity.this.getString(R.string.sprites_front),
+                    pokemon.getId() + PokemonDetailActivity.this.getString(R.string.extension)));
 
             // programmatically resize the height of ivSprite based on screen height
             double resize = 0.25;
-            int screenHeight = context.getResources().getDisplayMetrics().heightPixels;
+            int screenHeight = PokemonDetailActivity.this.getResources().getDisplayMetrics().heightPixels;
             ivSprite.getLayoutParams().height = (int) (screenHeight * resize);
 
-            float radius = context.getResources().getDimension(R.dimen.pokemon_detail_image_radius);
+            float radius = PokemonDetailActivity.this.getResources().getDimension(R.dimen.pokemon_detail_image_radius);
 
             ivOverlay.setShapeAppearanceModel(ivOverlay.getShapeAppearanceModel()
                     .toBuilder()
@@ -134,6 +133,9 @@ public class PokemonDetailActivity extends AppCompatActivity {
             tvLoading.setVisibility(View.VISIBLE);
         }
 
+        void enableButton() {
+            btnMoves.setEnabled(true);
+        }
 
         void afterDetails() {
 
@@ -145,51 +147,23 @@ public class PokemonDetailActivity extends AppCompatActivity {
             tvHabitat.setText(pokemon.getHabitat());
             tvHp.setText("Base HP: " + pokemon.getHp());
 
-            //Start the recyclerView
-            rvMoves.setAdapter(new MovesAdapter(pokemon.getMovesList()));
-        }
-    }
+            // Start parsing moves from DB
+            moves = pokemon.getMovesList();
 
-    class MovesAdapter extends RecyclerView.Adapter<MoveHolder> {
+            DaoThread daoThread = new DaoThread();
 
-        private List<Move> moveList;
+            daoThread.getMoveType(PokemonDetailActivity.this, moves, handler, this::enableButton);
 
-        MovesAdapter(List<Move> moveList) {
-            this.moveList = moveList;
-        }
-
-        @NonNull
-        @Override
-        public MoveHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            ConstraintLayout cl;
-            // Inflate row of RecyclerView
-            cl = (ConstraintLayout) LayoutInflater
-                    .from(parent.getContext())
-                    .inflate(R.layout.item_move, parent, false);
-
-            return new MoveHolder(cl);
         }
 
         @Override
-        public void onBindViewHolder(@NonNull MoveHolder holder, int position) {
-            Move move = moveList.get(position);
-
-            holder.tvMove.setText(move.getName());
-        }
-
-        @Override
-        public int getItemCount() {
-            return moveList.size();
+        public void onClick(View v) {
+            if (v.getId() == R.id.btnMoves) {
+                FragmentMoves fragmentMoves = new FragmentMoves();
+                fragmentMoves.setMoveList(moves);
+                fragmentMoves.show(getSupportFragmentManager(), FragmentMoves.TAG);
+            }
         }
     }
 
-    class MoveHolder extends RecyclerView.ViewHolder {
-        TextView tvMove;
-
-        public MoveHolder(@NonNull View itemView) {
-            super(itemView);
-
-            tvMove = itemView.findViewById(R.id.tvMove);
-        }
-    }
 }
---
+++ app/src/main/java/it/thetarangers/thetamon/fragments/FragmentMoves.java
@@ -0,0 +1,116 @@
+package it.thetarangers.thetamon.fragments;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.google.android.material.card.MaterialCardView;
+
+import java.util.List;
+
+import it.thetarangers.thetamon.R;
+import it.thetarangers.thetamon.model.Move;
+
+public class FragmentMoves extends BottomSheetDialogFragment {
+
+    public static String TAG = "FragmentMoves";
+    Holder holder;
+    private List<Move> moveList;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.moves_sheet_layout, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        holder = new Holder(view);
+
+        BottomSheetDialog dialog = (BottomSheetDialog) this.getDialog();
+        assert dialog != null;
+        BottomSheetBehavior<FrameLayout> behavior = dialog.getBehavior();
+        behavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+    }
+
+    public void setMoveList(List<Move> moveList) {
+        this.moveList = moveList;
+    }
+
+    static class MovesAdapter extends RecyclerView.Adapter<MoveHolder> {
+
+        private List<Move> moveList;
+
+        MovesAdapter(List<Move> moveList) {
+            this.moveList = moveList;
+        }
+
+        @NonNull
+        @Override
+        public MoveHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            MaterialCardView materialCardView;
+            // Inflate row of RecyclerView
+            materialCardView = (MaterialCardView) LayoutInflater
+                    .from(parent.getContext())
+                    .inflate(R.layout.item_move, parent, false);
+
+            return new MoveHolder(materialCardView);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MoveHolder holder, int position) {
+            Move move = moveList.get(position);
+
+            holder.tvMove.setText(move.getName());
+
+            //TODO set move type
+            Log.d("POKE", "move: " + move.getName() + " type: " + move.getType());
+        }
+
+        @Override
+        public int getItemCount() {
+            return moveList.size();
+        }
+    }
+
+    static class MoveHolder extends RecyclerView.ViewHolder {
+        TextView tvMove;
+
+        public MoveHolder(@NonNull View itemView) {
+            super(itemView);
+
+            tvMove = itemView.findViewById(R.id.tvMove);
+        }
+    }
+
+    class Holder implements View.OnClickListener {
+
+        Holder(View fm) {
+            RecyclerView rvMoves = fm.findViewById(R.id.rvMoves);
+            rvMoves.setLayoutManager(new LinearLayoutManager(requireContext()));
+            MovesAdapter movesAdapter = new MovesAdapter(moveList);
+            rvMoves.setAdapter(movesAdapter);
+        }
+
+        @Override
+        public void onClick(View v) {
+            // TODO
+        }
+
+    }
+
+}
