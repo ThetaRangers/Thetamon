632124bcc74667efd6af797db49b155c489a3365
ferra-rally <danieleferrarelli@gmail.com>
2020-06-12 16:29:18 +0200

    Added evolution chain parser

--- app/src/main/java/it/thetarangers/thetamon/model/EvolutionDetail.java
+++ app/src/main/java/it/thetarangers/thetamon/model/EvolutionDetail.java
@@ -1,24 +1,30 @@
 package it.thetarangers.thetamon.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EvolutionDetail {
     private String gender;
-    private String held_item;
-    private String item;
-    private String known_move;
-    private String known_move_type;
-    private String location;
-    private String min_affection;
-    private String min_beauty;
-    private String min_happiness;
-    private String min_level;
+    private Object held_item;
+    private Object item;
+    private Object known_move;
+    private Object knownMoveType;
+    private Object locationName;
+    private int min_affection;
+    private int min_beauty;
+    private int min_happiness;
+    private int min_level;
     private Boolean needs_overworld_rain;
-    private String party_species;
-    private String party_type;
-    private String relative_physical_stats;
+    private Object party_species;
+    private Object party_type;
+    private Object relative_physical_stats;
     private String time_of_day;
-    private String trade_species;
-    private String trigger;
-    private int nextPokemonId;
+    private Object trade_species;
+    private Object trigger;
+    private Boolean turn_upside_down;
+
+    private List<EvolutionDetail> nextPokemon;
+    private String name;
 
     public String getGender() {
         return gender;
@@ -28,75 +34,83 @@ public class EvolutionDetail {
         this.gender = gender;
     }
 
-    public String getHeld_item() {
+    public Object getHeld_item() {
         return held_item;
     }
 
-    public void setHeld_item(String held_item) {
+    public void setHeld_item(Object held_item) {
         this.held_item = held_item;
     }
 
-    public String getItem() {
+    public Object getItem() {
         return item;
     }
 
-    public void setItem(String item) {
+    public void setItem(Object item) {
         this.item = item;
     }
 
-    public String getKnown_move() {
+    public Object getKnown_move() {
         return known_move;
     }
 
-    public void setKnown_move(String known_move) {
+    public void setKnown_move(Object known_move) {
         this.known_move = known_move;
     }
 
-    public String getKnown_move_type() {
-        return known_move_type;
+    public Object getKnownMoveType() {
+        return knownMoveType;
     }
 
-    public void setKnown_move_type(String known_move_type) {
-        this.known_move_type = known_move_type;
+    public void setKnownMoveType(Object knownMoveType) {
+        this.knownMoveType = knownMoveType;
     }
 
-    public String getLocation() {
-        return location;
+    public Object getLocationName() {
+        return locationName;
     }
 
-    public void setLocation(String location) {
-        this.location = location;
+    public void setLocationName(Object locationName) {
+        this.locationName = locationName;
     }
 
-    public String getMin_affection() {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMin_affection() {
         return min_affection;
     }
 
-    public void setMin_affection(String min_affection) {
+    public void setMin_affection(int min_affection) {
         this.min_affection = min_affection;
     }
 
-    public String getMin_beauty() {
+    public int getMin_beauty() {
         return min_beauty;
     }
 
-    public void setMin_beauty(String min_beauty) {
+    public void setMin_beauty(int min_beauty) {
         this.min_beauty = min_beauty;
     }
 
-    public String getMin_happiness() {
+    public int getMin_happiness() {
         return min_happiness;
     }
 
-    public void setMin_happiness(String min_happiness) {
+    public void setMin_happiness(int min_happiness) {
         this.min_happiness = min_happiness;
     }
 
-    public String getMin_level() {
+    public int getMin_level() {
         return min_level;
     }
 
-    public void setMin_level(String min_level) {
+    public void setMin_level(int min_level) {
         this.min_level = min_level;
     }
 
@@ -108,27 +122,27 @@ public class EvolutionDetail {
         this.needs_overworld_rain = needs_overworld_rain;
     }
 
-    public String getParty_species() {
+    public Object getParty_species() {
         return party_species;
     }
 
-    public void setParty_species(String party_species) {
+    public void setParty_species(Object party_species) {
         this.party_species = party_species;
     }
 
-    public String getParty_type() {
+    public Object getParty_type() {
         return party_type;
     }
 
-    public void setParty_type(String party_type) {
+    public void setParty_type(Object party_type) {
         this.party_type = party_type;
     }
 
-    public String getRelative_physical_stats() {
+    public Object getRelative_physical_stats() {
         return relative_physical_stats;
     }
 
-    public void setRelative_physical_stats(String relative_physical_stats) {
+    public void setRelative_physical_stats(Object relative_physical_stats) {
         this.relative_physical_stats = relative_physical_stats;
     }
 
@@ -140,27 +154,39 @@ public class EvolutionDetail {
         this.time_of_day = time_of_day;
     }
 
-    public String getTrade_species() {
+    public Object getTrade_species() {
         return trade_species;
     }
 
-    public void setTrade_species(String trade_species) {
+    public void setTrade_species(Object trade_species) {
         this.trade_species = trade_species;
     }
 
-    public String getTrigger() {
+    public Object getTrigger() {
         return trigger;
     }
 
-    public void setTrigger(String trigger) {
+    public void setTrigger(Object trigger) {
         this.trigger = trigger;
     }
 
-    public int getNextPokemonId() {
-        return nextPokemonId;
+    public List<EvolutionDetail> getNextPokemon() {
+        return nextPokemon;
     }
 
-    public void setNextPokemonId(int nextPokemonId) {
-        this.nextPokemonId = nextPokemonId;
+    public void setNextPokemon(List<EvolutionDetail> nextPokemonList) {
+        this.nextPokemon = nextPokemonList;
+    }
+
+    public void setNextPokemon(EvolutionDetail nextPokemon){
+        this.nextPokemon.add(nextPokemon);
+    }
+
+    public Boolean getTurn_upside_down() {
+        return turn_upside_down;
+    }
+
+    public void setTurn_upside_down(Boolean turn_upside_down) {
+        this.turn_upside_down = turn_upside_down;
     }
 }
--- app/src/main/java/it/thetarangers/thetamon/utilities/VolleyEvolutionChain.java
+++ app/src/main/java/it/thetarangers/thetamon/utilities/VolleyEvolutionChain.java
@@ -1,6 +1,8 @@
 package it.thetarangers.thetamon.utilities;
 
 import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -8,20 +10,30 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import it.thetarangers.thetamon.R;
+import it.thetarangers.thetamon.model.EvolutionDetail;
 import it.thetarangers.thetamon.model.Pokemon;
 
 public abstract class VolleyEvolutionChain implements Response.ErrorListener, Response.Listener<String> {
     Context context;
+    Gson gson = new Gson();
 
-    public abstract void fill(Pokemon pokemon);
-
-    VolleyEvolutionChain(Context context){
+    protected VolleyEvolutionChain(Context context) {
         this.context = context;
     }
 
-    public void getEvolutionChain(Pokemon pokemon, String chain){
+    public abstract void fill(List<EvolutionDetail> evolutionDetails);
+
+    public void getEvolutionChain(Pokemon pokemon, String chain) {
         RequestQueue requestQueue;
         requestQueue = Volley.newRequestQueue(context);
 
@@ -34,11 +46,70 @@ public abstract class VolleyEvolutionChain implements Response.ErrorListener, Re
 
     @Override
     public void onErrorResponse(VolleyError error) {
-
+        Toast.makeText(context, R.string.volley_error, Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onResponse(String response) {
+        List<EvolutionDetail> evolutions = null;
 
+        try {
+            Log.d("POKE", "Madonna se trovo eevee");
+
+            JSONObject jsonObject = new JSONObject(response);
+
+            JSONObject chain = jsonObject.getJSONObject("chain");
+
+            //First evolution
+            EvolutionDetail firstEvolution = new EvolutionDetail();
+            firstEvolution.setName(chain.getJSONObject("species").getString("name"));
+
+            //Second evolution
+            JSONArray secondEvolvesTo = chain.getJSONArray("evolves_to");
+
+            List<EvolutionDetail> secondEvolutions = new ArrayList<>();
+
+            for (int i = 0; i < secondEvolvesTo.length(); i++) {
+                JSONObject obj = secondEvolvesTo.getJSONObject(i);
+
+                EvolutionDetail ev = parseEvolutionDetail(obj);
+
+                //Third evolutions
+                JSONArray thirdEvolutionJson = obj.getJSONArray("evolves_to");
+                if(thirdEvolutionJson.length() != 0) {
+                    List<EvolutionDetail> thirdEvolutions = new ArrayList<>();
+
+                    for(int j = 0; j < thirdEvolutionJson.length(); j++) {
+                        JSONObject object = thirdEvolutionJson.getJSONObject(j);
+
+                        EvolutionDetail evolutionDetail = parseEvolutionDetail(object);
+
+                        thirdEvolutions.add(evolutionDetail);
+                    }
+
+                    ev.setNextPokemon(thirdEvolutions);
+                }
+
+                secondEvolutions.add(ev);
+            }
+
+            firstEvolution.setNextPokemon(secondEvolutions);
+
+            Log.d("POKE", "Numero Evoluzioni 1:" + 1 + " 2: " + firstEvolution.getNextPokemon().size());
+
+        } catch (JSONException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    private EvolutionDetail parseEvolutionDetail(JSONObject obj) throws JSONException {
+
+        JSONObject tmp = obj.getJSONArray("evolution_details").getJSONObject(0);
+
+        EvolutionDetail ev = gson.fromJson(tmp.toString(), EvolutionDetail.class);
+
+        ev.setName(obj.getJSONObject("species").getString("name"));
+
+        return ev;
     }
 }
