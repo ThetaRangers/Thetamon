58e1f2394bbee28bb1a68d2b332b7ac0b8d18627
Lorenzo Valeriani <lorenzoval98@gmail.com>
2020-06-18 11:33:47 +0200

    Add FavoriteListViewModel
    Signed-off-by: Lorenzo Valeriani <lorenzoval98@gmail.com>

---
+++ app/src/main/java/it/thetarangers/thetamon/viewmodel/FavoriteListViewModel.java
@@ -0,0 +1,30 @@
+package it.thetarangers.thetamon.viewmodel;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.List;
+
+import it.thetarangers.thetamon.model.Pokemon;
+
+public class FavoriteListViewModel extends ViewModel {
+
+    private MutableLiveData<List<Pokemon>> pokemons;
+
+    public LiveData<List<Pokemon>> getFavorites() {
+        if (pokemons == null) {
+            pokemons = new MutableLiveData<>();
+        }
+        return pokemons;
+    }
+
+    public void setFavoritesAsynchronous(List<Pokemon> pokemons) {
+        this.pokemons.postValue(pokemons);
+    }
+
+    public List<Pokemon> getFavoriteList() {
+        return pokemons.getValue();
+    }
+
+}
